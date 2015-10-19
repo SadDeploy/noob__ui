4e3372ba5c29d1f4019b3f04d739d7f57587084c
ViralL <viirall@yandex.ru>
2015-10-19 21:10:14 +0300

    style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,31 @@
-const myApp = myApp || {};
 
-console.log('Hodd luck!');
+
+$(window).load(function() {
+    // animate
+    $(window).scroll(function() {
+        $('#benefits .container').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+900) {
+                $(this).addClass("slideUp");
+            }
+        });
+        $('#shares_anim .container').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+800) {
+                $(this).addClass("fadeIn");
+            }
+        });
+        $('#partners .container').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+700) {
+                $(this).addClass("bigEntrance");
+            }
+        });
+    });
+});
