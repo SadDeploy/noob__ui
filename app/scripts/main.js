ce448e1bd13b4377016151f03f454d5a6ac7e88c
ViralL <viirall@yandex.ru>
2016-01-13 09:35:06 +0300

    table bugs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,6 @@
 
+$(document).ready(function() {
+    // phone
+    $("[name=phone]").mask("+7 (999) 999-9999");
 
-$(window).load(function() {
-    // animate
-    $(window).scroll(function() {
-        $('#benefits .container').each(function(){
-            var imagePos = $(this).offset().top;
-
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+900) {
-                $(this).addClass("slideUp");
-            }
-        });
-        $('#shares_anim .container').each(function(){
-            var imagePos = $(this).offset().top;
-
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+800) {
-                $(this).addClass("fadeIn");
-            }
-        });
-        $('#partners .container').each(function(){
-            var imagePos = $(this).offset().top;
-
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+700) {
-                $(this).addClass("bigEntrance");
-            }
-        });
-    });
 });
